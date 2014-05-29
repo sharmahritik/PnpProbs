564594f686451656917a55d263cd55ec0d1bde2b
Yongtao Ye <ytye@cs.hku.hk>
2014-05-29 14:41:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 /**************************************************
- * # GLProbs: Aligning multiple sequences adaptively
+ * # PNPProbs: 
  * #
  * # Contact: YE Yongtao, Department of Computer Science,
  * #			 The University of Hong Kong.
  * # Emails:	 ytye@cs.hku.hk
  * # 
- * # 0%-25%  identity: global pair-HMM + local pair-HMM + double affine pair-HMM
- * # 25%-40% identity: local pair-HMM
- * # >40%    identity: global pair-HMM
  * #
- * # This program is developed based on the open source code of MSAProbs.
  * ************************************************/
 #include "MSA.h"
 
